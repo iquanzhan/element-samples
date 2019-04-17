81e4fe5297d73898471b1bb38e3b6a675725e1c2
iquanzhan <iquanzhan@163.com>
2019-04-17 16:07:43 +0800

    menuconfig

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,62 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <span v-html="initMenuConfig(this.menuConfig)"></span>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import menuConfig from "./config/menuConfig.js";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      menuConfig: menuConfig
+    };
+  },
+  methods: {
+    handleOpen: () => {},
+    handleClose: () => {},
+    initMenuConfig: config => {
+      return (
+        config &&
+        config.map((item, index) => {
+          if (!item.children) {
+            return (
+              <el-menu-item index={item.key}>
+                <i class="el-icon-setting" />
+                <span slot="title">{item.title}</span>
+              </el-menu-item>
+            );
+          } else {
+            return (
+              <el-menu-item-group>
+                {this.initMenuConfig(item.children)}
+              </el-menu-item-group>
+            );
+          }
+        })
+      );
+    }
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
